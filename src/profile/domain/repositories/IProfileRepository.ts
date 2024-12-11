aa26c10dfa495b06d2b7a0eac86cc5b780ae2cf1
ThePixel256 <pixelbrack@gmail.com>
2024-12-11 16:52:11 -0500

    feat(profile): add interface and class for repository

---
+++ src/profile/domain/repositories/IProfileRepository.ts
@@ -0,0 +1,10 @@
+import {Profile} from "../model/aggregates/Profile";
+import {Nullable} from "../../../shared/domain/types/Nullable";
+
+export interface IProfileRepository {
+    save(profile: Profile): Promise<Profile>;
+    update(profile: Profile): Promise<Nullable<Profile>>;
+    delete(profileId: number): Promise<Nullable<number>>;
+    findById(profileId: number): Promise<Nullable<Profile>>;
+    findByUserId(userId: number): Promise<Nullable<Profile>>;
+}
---
+++ src/profile/infrastructure/persistence/orm/repositories/ProfileRepository.ts
@@ -0,0 +1,38 @@
+import {TypeOrmRepository} from "../../../../../shared/infrastructure/persistence/orm/repositories/TypeOrmRepository";
+import {Profile} from "../../../../domain/model/aggregates/Profile";
+import {IProfileRepository} from "../../../../domain/repositories/IProfileRepository";
+import {Nullable} from "../../../../../shared/domain/types/Nullable";
+import {UserId} from "../../../../domain/model/valueobjects/UserId";
+
+export class ProfileRepository extends TypeOrmRepository<Profile> implements IProfileRepository {
+    protected entitySchema(): Function {
+        return Profile;
+    }
+    async save(profile: Profile): Promise<Profile> {
+        const repository = await this.repository();
+        return repository.save(profile);
+    }
+
+    async update(profile: Profile): Promise<Nullable<Profile>> {
+        const repository = await this.repository();
+        await repository.update(profile.id, profile);
+        return await repository.findOne({ where: { id: profile.id } });
+    }
+
+    async delete(profileId: number): Promise<Nullable<number>> {
+        const repository = await this.repository();
+        const result = await repository.delete(profileId);
+        return result.affected ? result.affected : null;
+    }
+
+    async findById(profileId: number): Promise<Nullable<Profile>> {
+        const repository = await this.repository();
+        return repository.findOne({ where: { id: profileId } });
+    }
+
+    async findByUserId(userId: number): Promise<Nullable<Profile>> {
+        const repository = await this.repository();
+        const userIdValue = new UserId(userId);
+        return repository.findOne({ where: { userId: userIdValue } });
+    }
+}
