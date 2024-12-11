fb3d7328f6101a39666a6c0ed9d1becd224a6440
ThePixel256 <pixelbrack@gmail.com>
2024-12-11 01:23:55 -0500

    feat: update user entity

---
+++ src/iam/domain/model/aggregates/User.ts
@@ -0,0 +1,29 @@
+import { AggregateRoot } from "../../../../shared/domain/model/aggregates/AggregateRoot";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity('users')
+export class User extends AggregateRoot {
+    @PrimaryGeneratedColumn()
+    readonly id!: number;
+
+    @Column({ unique: true })
+    public username!: string;
+
+    @Column()
+    private password!: string;
+
+    constructor(username?: string, password?: string) {
+        super();
+        if (username && password) {
+            this.username = username;
+            this.password = password;
+        }
+    }
+
+    changePassword(newPassword: string): void {
+        if (newPassword.length < 8) {
+            throw new Error('Password must be at least 8 characters long');
+        }
+        this.password = newPassword;
+    }
+}
