5504637d36ffda4c75c9b1d147b221fcd23f9f64
ThePixel256 <pixelbrack@gmail.com>
2024-12-11 05:55:20 -0500

    feat(app): add board repository, services and controllers

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ import {UserCommandService} from "./iam/application/internal/commandservices/Use
 import {UserQueryService} from "./iam/application/internal/queryservices/UserQueryService";
 import {AuthenticationController} from "./iam/interfaces/rest/AuthenticationController";
 import {swaggerConfig} from "./shared/infrastructure/documentation/SwaggerConfig";
+import {BoardRepository} from "./board/infrastructure/persistence/orm/repositories/BoardRepository";
+import {BoardCommandService} from "./board/application/internal/commandservices/BoardCommandService";
+import {BoardQueryService} from "./board/application/internal/queryservices/BoardQueryService";
+import {BoardController} from "./board/interfaces/rest/BoardController";
+import {BoardMemberController} from "./board/interfaces/rest/BoardMemberController";
 
 export const createApp = (): Application => {
     const app = express();
@@ -24,8 +29,16 @@ export const createApp = (): Application => {
     const userQueryService = new UserQueryService(userRepository);
     const userController = new AuthenticationController(userCommandService);
 
+    const boardRepository = new BoardRepository(AppDataSource);
+    const boardCommandService = new BoardCommandService(boardRepository);
+    const boardQueryService = new BoardQueryService(boardRepository);
+    const boardController = new BoardController(boardCommandService, boardQueryService);
+    const boardMemberController = new BoardMemberController(boardCommandService, boardQueryService);
+
     // Routes
     app.use('/api/v1/authentication', userController.getRoutes());
+    app.use('/api/v1/boards', boardController.getRoutes());
+    app.use('/api/v1/boards', boardMemberController.getRoutes());
 
     // Swagger UI
     swaggerConfig(app);
