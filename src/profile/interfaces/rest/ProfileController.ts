4edda77b5c691738e0ba6d4573826e120760d874
ThePixel256 <pixelbrack@gmail.com>
2024-12-12 18:37:27 -0500

    feat(profile): update controller

--- src/profile/interfaces/rest/ProfileController.ts
+++ src/profile/interfaces/rest/ProfileController.ts
@@ -3,6 +3,7 @@ import {ProfileCommandService} from "../../application/internal/commandservices/
 import {ProfileQueryService} from "../../application/internal/queryservices/ProfileQueryService";
 import {ProfileResourceFromEntityAssembler} from "./transform/ProfileResourceFromEntityAssembler";
 import {GetProfileByUserIdQuery} from "../../domain/model/queries/GetProfileByUserIdQuery";
+import {GetAllProfilesQuery} from "../../domain/model/queries/GetAllProfilesQuery";
 
 export class ProfileController {
     private readonly router: Router;
@@ -17,52 +18,59 @@ export class ProfileController {
          * @swagger
          * /api/v1/profiles:
          *   get:
-         *     summary: Get a profile by user ID
+         *     summary: Get profiles
          *     tags: [Profiles]
-         *     description: Retrieve a profile by its user ID.
+         *     description: Retrieve all profiles or a specific profile by its user ID.
          *     parameters:
          *       - in: query
          *         name: userId
-         *         required: true
          *         schema:
          *           type: integer
+         *         required: false
          *         description: The user ID of the profile to retrieve.
          *     responses:
          *       200:
-         *         description: Profile retrieved successfully.
+         *         description: List of profiles or a specific profile retrieved successfully.
          *         content:
          *           application/json:
          *             schema:
-         *               $ref: '#/components/schemas/ProfileResource'
+         *               oneOf:
+         *                 - type: array
+         *                   items:
+         *                     $ref: '#/components/schemas/ProfileResource'
+         *                 - $ref: '#/components/schemas/ProfileResource'
          *       400:
-         *         description: Bad Request.
+         *         description: Bad Request. Missing or invalid parameters.
          *       404:
          *         description: Profile not found.
          *       500:
          *         description: Internal Server Error.
          */
-        this.router.get('', this.getProfileByUserId.bind(this));
+        this.router.get('', this.getProfiles.bind(this));
     }
 
     public getRoutes() {
         return this.router;
     }
 
-    async getProfileByUserId(req: any, res: any) {
+    async getProfiles(req: any, res: any) {
         try {
             const userId = req.query.userId;
             if (!userId) {
-                res.status(400).json({error: 'Bad Request', details: 'Missing user ID'});
-                return;
+                const getAllProfilesQuery = new GetAllProfilesQuery();
+                const profiles = await this.profileQueryService.getAllProfiles(getAllProfilesQuery);
+                const profileResources = profiles.map(profile => ProfileResourceFromEntityAssembler.toResourceFromEntity(profile));
+                res.status(200).send(profileResources);
+            } else {
+                const getProfileByUserId = new GetProfileByUserIdQuery(userId);
+                const profile = await this.profileQueryService.getProfileByUserId(getProfileByUserId);
+                if (!profile) {
+                    res.status(404).json({error: 'Profile not found', details: `Profile with user ID ${userId} not found`});
+                    return;
+                }
+                const profileResource = ProfileResourceFromEntityAssembler.toResourceFromEntity(profile);
+                res.status(200).send(profileResource);
             }
-            const getProfileByUserId = new GetProfileByUserIdQuery(userId);
-            const profile = await this.profileQueryService.getProfileByUserId(getProfileByUserId);
-            if (!profile) {
-                res.status(404).json({error: 'Profile not found', details: `Profile with user ID ${userId} not found`});
-                return;
-            }
-            const profileResource = ProfileResourceFromEntityAssembler.toResourceFromEntity(profile);
-            res.status(200).send(profileResource);
         } catch (error) {
             const err = error as Error;
             console.error('Error:', err.message);
