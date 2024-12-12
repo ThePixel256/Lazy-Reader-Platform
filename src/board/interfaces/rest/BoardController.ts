aff270f1970984ebae1837dce89d3ce31ea83fd7
ThePixel256 <pixelbrack@gmail.com>
2024-12-12 01:41:07 -0500

    feat(board): update previos get all boards in controller

--- src/board/interfaces/rest/BoardController.ts
+++ src/board/interfaces/rest/BoardController.ts
@@ -4,7 +4,8 @@ import {BoardQueryService} from "../../application/internal/queryservices/BoardQ
 import {CreateBoardResource} from "./resources/CreateBoardResource";
 import {CreateBoardCommandFromResourceAssembler} from "./transform/CreateBoardCommandFromResourceAssembler";
 import {BoardResourceFromEntityAssembler} from "./transform/BoardResourceFromEntityAssembler";
-import {GetAllBoardsByUserIdQuery} from "../../domain/model/queries/GetAllBoardsByUserIdQuery";
+import {GetAllBoardsByOwnerIdQuery} from "../../domain/model/queries/GetAllBoardsByOwnerIdQuery";
+import {GetAllBoardsByMemberIdQuery} from "../../domain/model/queries/GetAllBoardsByMemberIdQuery";
 
 export class BoardController {
     private readonly router: Router;
@@ -45,18 +46,24 @@ export class BoardController {
          * @swagger
          * /api/v1/boards:
          *   get:
-         *     summary: Get all boards by user ID
+         *     summary: Get all boards
          *     tags: [Boards]
          *     parameters:
          *       - in: query
-         *         name: userId
-         *         required: true
+         *         name: ownerId
          *         schema:
-         *           type: integer
-         *         description: The ID of the user to fetch boards for
+         *           type: string
+         *         required: false
+         *         description: The user ID of the board owner
+         *       - in: query
+         *         name: memberId
+         *         schema:
+         *           type: string
+         *         required: false
+         *         description: The user ID of the board member
          *     responses:
          *       200:
-         *         description: List of boards for the user
+         *         description: Boards retrieved successfully
          *         content:
          *           application/json:
          *             schema:
@@ -65,12 +72,10 @@ export class BoardController {
          *                 $ref: '#/components/schemas/BoardResource'
          *       400:
          *         description: Bad Request
-         *       404:
-         *         description: User not found
          *       500:
          *         description: Internal Server Error
          */
-        this.router.get('', this.getAllBoardsByUserId.bind(this));
+        this.router.get('', this.getAllBoards.bind(this));
     }
 
     public getRoutes() {
@@ -91,15 +96,20 @@ export class BoardController {
         }
     }
 
-    async getAllBoardsByUserId(req: any, res: any) {
+    async getAllBoards(req: any, res: any) {
+        const ownerId = req.query.ownerId;
+        const memberId = req.query.memberId;
+        if (!ownerId && !memberId) {
+            res.status(400).json({error: 'Bad Request', details: 'Missing user ID'});
+            return;
+        }
+        if (ownerId && memberId) {
+            res.status(400).json({error: 'Bad Request', details: 'Only one of owner ID or member ID should be provided'});
+            return;
+        }
         try {
-            const userId = req.query.userId;
-            if (!userId) {
-                res.status(400).json({error: 'Bad Request', details: 'Missing user ID'});
-                return;
-            }
-            const getAllBoardsByUserId = new GetAllBoardsByUserIdQuery(userId);
-            const boards = await this.boardQueryService.getAllBoardsByUserId(getAllBoardsByUserId);
+            let query = ownerId ? new GetAllBoardsByOwnerIdQuery(ownerId) : new GetAllBoardsByMemberIdQuery(memberId);
+            const boards = query instanceof GetAllBoardsByOwnerIdQuery ? await this.boardQueryService.getAllBoardsByOwnerId(query) : await this.boardQueryService.getAllBoardsByMemberId(query);
             const boardResources = boards.map(board => BoardResourceFromEntityAssembler.toResourceFromEntity(board));
             res.status(200).send(boardResources);
         } catch (error) {
