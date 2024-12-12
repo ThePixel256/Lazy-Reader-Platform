0941c139835450d6a14218ebec9ad89c913d88b9
ThePixel256 <pixelbrack@gmail.com>
2024-12-11 20:14:09 -0500

    feat(iam): update command service by implementing unit of work and external profile service

--- src/iam/application/internal/commandservices/UserCommandService.ts
+++ src/iam/application/internal/commandservices/UserCommandService.ts
@@ -3,9 +3,15 @@ import {SignInCommand} from "../../../domain/model/commands/SignInCommand";
 import {SignUpCommand} from "../../../domain/model/commands/SignUpCommand";
 import {User} from "../../../domain/model/aggregates/User";
 import {UserRepository} from "../../../infrastructure/persistence/orm/repositories/UserRepository";
+import {ExternalProfileService} from "../outboundservices/acl/ExternalProfileService";
+import {UnitOfWork} from "../../../../shared/infrastructure/persistence/orm/repositories/UnitOfWork";
 
 export class UserCommandService implements IUserCommandService {
-    constructor(private userRepository: UserRepository) {
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly externalProfileService: ExternalProfileService,
+        private readonly unitOfWork: UnitOfWork
+    ) {
     }
 
     async signIn(command: SignInCommand): Promise<User> {
@@ -17,8 +23,34 @@ export class UserCommandService implements IUserCommandService {
     }
 
     async signUp(command: SignUpCommand): Promise<User> {
-        const newUser = new User(command.username, command.password);
-        return this.userRepository.save(newUser);
+        let savedUser: User | null = null;
+
+        try {
+            await this.unitOfWork.startTransaction();
+
+            const transactionalManager = this.unitOfWork.getManager();
+            const userRepository = transactionalManager.getRepository(User);
+
+            const newUser = new User(command.username, command.password);
+            savedUser = await userRepository.save(newUser);
+
+            await this.externalProfileService.createProfile(
+                command.firstName,
+                command.lastName,
+                command.email,
+                savedUser.id
+            );
+
+            await this.unitOfWork.commit();
+        } catch (error) {
+            console.error("Error during transaction:", error);
+            await this.unitOfWork.rollback();
+            throw new Error(`Transaction failed: ${(error as Error).message}`);
+        } finally {
+            await this.unitOfWork.release();
+        }
+
+        return savedUser!;
     }
 
 }
