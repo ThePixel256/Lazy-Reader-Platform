2573afaf79686f1faff411565501ed44dcf6b621
ThePixel256 <pixelbrack@gmail.com>
2024-12-12 20:23:11 -0500

    feat(iam): update authenticated user transform

--- src/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.ts
+++ src/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.ts
@@ -2,7 +2,7 @@ import {User} from "../../../domain/model/aggregates/User";
 import {AuthenticatedUserResource} from "../resources/AuthenticatedUserResource";
 
 export class AuthenticatedUserResourceFromEntityAssembler {
-    static toResourceFromEntity(entity: User) : AuthenticatedUserResource{
-        return new AuthenticatedUserResource(entity.id, entity.username, 'no token yet');
+    static toResourceFromEntity(entity: {user: User, token: string}) : AuthenticatedUserResource{
+        return new AuthenticatedUserResource(entity.user.id, entity.user.username, entity.token);
     }
 }
